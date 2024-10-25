e7b750027f9d8abdfcc491c177656dd8292c753d
Levin Bicer <lbi145937@stud.gibb.ch>
2024-10-25 15:14:54 +0200

    added history, gallery and sightseeing and did some responsiveness

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -16,5 +16,46 @@ import {Button} from "primeng/button";
   styleUrl: './gallery.component.css'
 })
 export class GalleryComponent {
-
+    products: any = [
+        {
+            name: "Basel inner City, Basel",
+            image: "basel_stadt.jpg"
+        },
+        {
+            name: "Bern Mountains, Berne",
+            image: "berner_oberland.jpg"
+        },
+        {
+            name: "Graubünden, Graubünden",
+            image: "graubuenden.jpg"
+        },
+        {
+            name: "Andermatt Gondola, Uri",
+            image: "andermatt.jpg"
+        }
+        ,
+        {
+            name: "Campione d'italia, Tessin",
+            image: "campione_d_itialia.jpg"
+        }
+        ,
+        {
+            name: "Lake of Cauma, Graubünden",
+            image: "caumasee.jpg"
+        }
+        ,
+        {
+            name: "Swiss Alps, Switzerland",
+            image: "alpenblick.jpg"
+        }
+        ,
+        {
+            name: "Trümmelbachfälle, Berne",
+            image: "truemmelbachfaelle.jpg"
+        },
+        {
+            name: "Lake of Vierwaldstätter, Lucerne",
+            image: "vierwaldstaettersee.jpg"
+        }
+    ]
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-header',
   standalone: true,
     imports: [
-        RouterLink
+        RouterLink,
+        NgIf
     ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+    isMenuOpen = false;
 
+    toggleMenu() {
+        this.isMenuOpen = !this.isMenuOpen;
+    }
 }
