25cc236649c4aa4f50e5ff926df158215aea798c
Levin Bicer <lbi145937@stud.gibb.ch>
2024-10-25 11:57:27 +0200

    tried adding carousel => not finished

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { CarouselModule } from 'primeng/carousel';
+import {TagModule} from "primeng/tag";
+import {Button} from "primeng/button";
+
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [],
+    imports: [
+        CarouselModule,
+        TagModule,
+        Button
+    ],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
